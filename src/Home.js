ca6a02faff437de67c36a90d8c0aa027eeecda0c
Lucas Pannuto <lucaspannuto@gmail.com>
2020-09-11 15:31:03 -0300

    Clases

--- src/Home.js
+++ src/Home.js
@@ -22,25 +22,88 @@ function Home() {
                 <img src={Icono1} alt="Icono1" />
                 <h3>CARDIO WORKOUT</h3>
                 <p>Aaslk;dfjaslk;dflkasjdfkljasdlkflkasjdfkljasdlkf</p>
-                <a href="/">Read More</a>
+                <a href="/">READ MORE</a>
               </div>
               <div className="tarjeta">
                 <img src={Icono2} alt="Icono2" />
                 <h3>STREING WORKOUT</h3>
                 <p>Aaslk;dfjaslk;dflkasjdfkflkasjdfkljasdlkljasdlkf</p>
-                <a href="/">Read More</a>
+                <a href="/">READ MORE</a>
               </div>
               <div className="tarjeta">
                 <img src={Icono3} alt="Icono3" />
                 <h3>PUMP WORKOUT</h3>
                 <p>Aaslk;dfjaslk;dflkasjdfklflkasjdfkljasdlkjasdlkf</p>
-                <a href="/">Read More</a>
+                <a href="/">READ MORE</a>
               </div>
             </div>
           </div>
         </section>
 
-        <section id="Classes"></section>
+        <section id="classes">
+          <div className="contenedor">
+            <h2>CLASES</h2>
+            <table>
+              <th>DAYS</th>
+              <th>MONDAY</th>
+              <th>TUESDAY</th>
+              <th>WEDNESDAY</th>
+              <th>THUSDAY</th>
+              <th>FRIDAY</th>
+              <tr className="gris">
+                <td className="horario">09.00 - 10.00</td>
+                <td></td>
+                <td></td>
+                <td>
+                  <p className="negrita">Yoga</p>
+                  <p>Michael Lewis</p>
+                </td>
+                <td>
+                  <p className="negrita">Zumba</p>
+                  <p>Donal Trump</p>
+                </td>
+                <td>
+                  <p className="negrita">Body Balance</p>
+                  <p>Gloria Ami</p>
+                </td>
+              </tr>
+              <tr className="blanco">
+                <td className="horario">10.00 - 12.00</td>
+                <td>
+                  <p className="negrita">Zumba</p>
+                  <p>Donal Trump</p>
+                </td>
+                <td>
+                  <p className="negrita">Body Balance</p>
+                  <p>Gloria Ami</p>
+                </td>
+                <td></td>
+                <td></td>
+                <td>
+                  <p className="negrita">Yoga</p>
+                  <p>Michael Lewis</p>
+                </td>
+              </tr>
+              <tr className="gris">
+                <td className="horario">12.00 - 14.00</td>
+                <td>
+                  <p className="negrita">Body Balance</p>
+                  <p>Gloria Ami</p>
+                </td>
+                <td>
+                  <p className="negrita">Zumba</p>
+                  <p>Donal Trump</p>
+                </td>
+                <td></td>
+                <td>
+                  <p className="negrita">Yoga</p>
+                  <p>Michael Lewis</p>
+                </td>
+                <td></td>
+              </tr>
+            </table>
+          </div>
+        </section>
       </body>
     </div>
   );
