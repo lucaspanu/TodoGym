5642daead6a7283a91aee919ffc0f8ac0b7253e3
Lucas Pannuto <lucaspannuto@gmail.com>
2020-09-11 00:18:40 -0300

    Header

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "./Header.css";
+import Logo from "./assets/Logo.png";
 
 function Header() {
   return (
     <div>
       <div className="contenedor">
         <div className="menu">
-          <img src="./assets/Logo.png" alt="logo" />
+          <img src={Logo} alt="logo" />
           <nav>
             <ul>
               <li>
-                <a href="">Home</a>
+                <a href="/">Home</a>
               </li>
               <li>
-                <a href="">Classes</a>
+                <a href="/">Classes</a>
               </li>
               <li>
-                <a href="">Trainers</a>
+                <a href="/">Trainers</a>
               </li>
               <li>
-                <a href="">Contacto</a>
+                <a href="/">Contacto</a>
               </li>
             </ul>
           </nav>
