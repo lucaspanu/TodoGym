a531ff38f77c173243b7bbd6644d5e3be3e4be09
Lucas Pannuto <lucaspannuto@gmail.com>
2020-09-11 02:33:06 -0300

    Tarjetas

--- src/Header.js
+++ src/Header.js
@@ -5,27 +5,31 @@ import Logo from "./assets/Logo.png";
 function Header() {
   return (
     <div>
-      <div className="contenedor">
-        <div className="menu">
-          <img src={Logo} alt="logo" />
-          <nav>
-            <ul>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/">Classes</a>
-              </li>
-              <li>
-                <a href="/">Trainers</a>
-              </li>
-              <li>
-                <a href="/">Contacto</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <body>
+        <header>
+          <div className="contenedor-header">
+            <div className="menu">
+              <img src={Logo} alt="logo" />
+              <nav>
+                <ul>
+                  <li>
+                    <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <a href="/">Classes</a>
+                  </li>
+                  <li>
+                    <a href="/">Trainers</a>
+                  </li>
+                  <li>
+                    <a href="/">Contacto</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+      </body>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,49 @@
 import React from "react";
 import "./Home.css";
 
+import Icono1 from "./assets/Iconos/Icon1.png";
+import Icono2 from "./assets/Iconos/Icon2.png";
+import Icono3 from "./assets/Iconos/Icon3.png";
+
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <body>
+        <header>
+          <div className="titular">
+            <h1>EMPIEZA A ENTRENAR CON NOSOTROS</h1>
+            <a href="/">Registrate</a>
+          </div>
+        </header>
+        <section id="tarjetas">
+          <div className="contenedor">
+            <div className="contenedor-tarjetas">
+              <div className="tarjeta">
+                <img src={Icono1} alt="Icono1" />
+                <h3>CARDIO WORKOUT</h3>
+                <p>Aaslk;dfjaslk;dflkasjdfkljasdlkflkasjdfkljasdlkf</p>
+                <a href="/">Read More</a>
+              </div>
+              <div className="tarjeta">
+                <img src={Icono2} alt="Icono2" />
+                <h3>STREING WORKOUT</h3>
+                <p>Aaslk;dfjaslk;dflkasjdfkflkasjdfkljasdlkljasdlkf</p>
+                <a href="/">Read More</a>
+              </div>
+              <div className="tarjeta">
+                <img src={Icono3} alt="Icono3" />
+                <h3>PUMP WORKOUT</h3>
+                <p>Aaslk;dfjaslk;dflkasjdfklflkasjdfkljasdlkjasdlkf</p>
+                <a href="/">Read More</a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="Classes"></section>
+      </body>
+    </div>
+  );
 }
 
 export default Home;
