3cefa6d974556787be653248595f57bcffe88976
Lucas Pannuto <lucaspannuto@gmail.com>
2020-09-10 21:32:20 -0300

    Logos

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 
 import Home from "./Home";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <home />
+      <Header />
+      <Home />
     </div>
   );
 }
