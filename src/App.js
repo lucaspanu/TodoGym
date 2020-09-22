5ac8ddb8078d9526165ccc609dc235b0f1723f4b
Lucas Pannuto <lucaspannuto@gmail.com>
2020-09-22 20:09:50 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import Home from "./Home";
 import Header from "./Header";
 
+import Home2 from "./Home2";
+import Contact from "./Contact";
+import NoMatch from "./NoMatch";
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      {/* <Header />
+      <Home /> */}
+
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home2} />
+            <Route exact path="/Contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </React.Fragment>
     </div>
   );
 }
---
+++ src/Home2.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home2() {
+  return (
+    <div>
+      <h2>Hello Word</h2>
+      <p>This is an example of a simple HTML page with one paragraph.</p>
+      <p>This is an example of a simple HTML page with one paragraph.</p>
+    </div>
+  );
+}
+
+export default Home2;
