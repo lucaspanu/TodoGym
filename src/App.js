fac439fc117a0487f052b4578c439726dc5db754
lucasdmadrigal <lucasmadrigalb@gmail.com>
2020-09-23 15:23:44 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NoMatch from "./NoMatch";
 import Layout from "./components/Layout";
 import NavigationBar from "./components/NavigationBar";
 import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           </Router>
         </Layout>
       </React.Fragment>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/Styles/Footer.css";
+function Footer() {
+  return (
+    <footer className="footer">
+      <a href="/">Sobre Nosotros</a>
+      <a href="/">Terminos de servicio</a>
+      <a href="/">Politica de privacidad</a>
+      <a href="/">productos</a>
+      <a href="/">contactos</a>
+    </footer>
+  );
+}
+
+export default Footer;
