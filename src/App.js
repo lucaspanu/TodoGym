da816f3009c0786ed8cfb1ed097099ed1be5da4e
Lucas Pannuto <lucaspannuto@gmail.com>
2020-09-25 01:51:05 -0300

    Limpieza, Restart

--- src/App.js
+++ src/App.js
@@ -2,33 +2,29 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import Home from "./Home";
-import Header from "./Header";
-
-import Home2 from "./Home2";
+import Home from "./Home2";
 import Contact from "./Contact";
 import NoMatch from "./NoMatch";
 
-import Layout from "./components/Layout";
-import NavigationBar from "./components/NavBar3";
-import Jumbotron from "./components/Jumbotron";
+// import NavigationBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <React.Fragment>
-        <NavigationBar />
+        {/* <NavigationBar /> */}
 
         <Router>
           <Switch>
-            <Route exact path="/" component={Home2} />
+            <Route exact path="/" component={Home} />
             <Route exact path="/contact" component={Contact} />
             <Route component={NoMatch} />
           </Switch>
         </Router>
+
+        <Footer />
       </React.Fragment>
-      <Footer />
     </div>
   );
 }
