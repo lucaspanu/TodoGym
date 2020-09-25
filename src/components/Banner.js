146e5f4e3c28f872bcd18f4d5431f9d0b61baae7
lucasdmadrigal <lucasmadrigalb@gmail.com>
2020-09-25 11:43:52 -0300

    Banner terminado

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "../assets/Styles/Banner.css";
 function Banner() {
-  return <div className="Banner"></div>;
+  return (
+    <div className="Banner">
+      <div className="Container">
+        <div>
+          <h2 className="core-title">INSCRIBITE AHORA Y EMPEZA A ENTRENAR</h2>
+          <h3>unite a nuestros mas de 100 usuarios</h3>
+        </div>
+        <div>
+          <button className="button">Sing Up </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
