334ea471c2552f9bd9b476e63fe5a0fb9e3ae391
Lucas Pannuto <lucaspannuto@gmail.com>
2020-09-25 01:58:17 -0300

    Reinicio

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import Home from "./Home2";
+import Home from "./Home";
 import Contact from "./Contact";
 import NoMatch from "./NoMatch";
 
