d916ec5201b01d5c60727d14cafccc9e5b90cd90
lucasdmadrigal <lucasmadrigalb@gmail.com>
2020-09-29 03:37:35 -0300

    nuevo contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,35 +1,22 @@
 import React from "react";
-import "../../components/Contact.css";
+import "../../components/Contact_2.css";
 import Footer from "../Footer";
 
 function Contact() {
   return (
-    <div>
-      <div className="Container">
-        <div className="Container_General">
-          <div className="Cards_General">
-            <h2>Contactanos</h2>
-            <p>Mail:</p>
-          </div>
+    <div className="container_general">
+      <div className="container_title">
+        <div className="container_title_principal">
+          <h2>Te esperamos en calquiera de nuestras sucursales</h2>
         </div>
-
-        <div className="Container_Cards">
-          <div className="Cards">
-            <h2>Sede Capital</h2>
-            <p>Direccion:</p>
-            <p>Telefono:</p>
-          </div>
-          <div className="Cards">
-            <h2>Sede Yerba Buena</h2>
-            <p>Direccion:</p>
-            <p>Telefono:</p>
-          </div>
-          <div className="Cards">
-            <h2>Sede La Pampa</h2>
-            <p>Direccion:</p>
-            <p>Telefono:</p>
-          </div>
+        <div className=" container_subtitle">
+          <p>Todas totalmente equipadas para darte las mejores experiencias</p>
         </div>
+      </div>
+      <div className="social_items">
+        <div className="image_social"></div>
+        <div className="image_social"></div>
+        <div className="image_social"></div>
       </div>
       <Footer />
     </div>
