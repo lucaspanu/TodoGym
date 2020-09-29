7c26fbc56fddd947a32225442a3f743223868643
lucasdmadrigal <lucasmadrigalb@gmail.com>
2020-09-29 04:04:10 -0300

    arreglos en contactos..me gusta bastante - faltan los detalles

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,16 +7,23 @@ function Contact() {
     <div className="container_general">
       <div className="container_title">
         <div className="container_title_principal">
-          <h2>Te esperamos en calquiera de nuestras sucursales</h2>
+          <h1>Te esperamos en calquiera de nuestras sucursales</h1>
         </div>
         <div className=" container_subtitle">
-          <p>Todas totalmente equipadas para darte las mejores experiencias</p>
+          <h5>
+            Todas totalmente equipadas para darte las mejores experiencias
+          </h5>
         </div>
       </div>
       <div className="social_items">
         <div className="image_social"></div>
         <div className="image_social"></div>
         <div className="image_social"></div>
+      </div>
+      <div className="container_sedes">
+        <div className="item_sedes"></div>
+        <div className="item_sedes"></div>
+        <div className="item_sedes"></div>
       </div>
       <Footer />
     </div>
