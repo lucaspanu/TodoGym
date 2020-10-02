1969a74dc5d00b223430d6884bb24933db57cb6a
lucasdmadrigal <lucasmadrigalb@gmail.com>
2020-10-02 14:33:00 -0300

    iconos agregados de redes y sucursales

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../../components/Contact_2.css";
+import icono from "../../assets/Iconos/map-pointer.png";
 import Footer from "../Footer";
+import { Link } from "react-router-dom";
 
 function Contact() {
   return (
@@ -16,14 +18,46 @@ function Contact() {
         </div>
       </div>
       <div className="social_items">
-        <div className="image_social"></div>
-        <div className="image_social"></div>
-        <div className="image_social"></div>
+        <div className="image_social">
+          <Link to="/" target="_blank" className="social-icon facebook">
+            <i class="fab fa-facebook" />
+          </Link>
+        </div>
+        <div className="image_social">
+          <Link to="/" target="_blank" className="social-icon instagram">
+            <i class="fab fa-instagram" />
+          </Link>
+        </div>
+        <div className="image_social">
+          <Link to="/" target="_blank" className="social-icon youtube">
+            <i class="fab fa-youtube" />
+          </Link>
+        </div>
       </div>
       <div className="container_sedes">
-        <div className="item_sedes"></div>
-        <div className="item_sedes"></div>
-        <div className="item_sedes"></div>
+        <div className="item_sedes">
+          {/* <div className="center">
+            <div className="pointer"></div>
+            <div className="shadow"></div>
+          </div> */}
+
+          <img className="sede_icon" src={icono} alt="icono" />
+          <div>
+            <h2>Capital</h2>
+          </div>
+        </div>
+        <div className="item_sedes">
+          <img className="sede_icon" src={icono} alt="icono" />
+          <div>
+            <h2>Yerba Buena</h2>
+          </div>
+        </div>
+        <div className="item_sedes">
+          <img className="sede_icon" src={icono} alt="icono" />
+          <div>
+            <h2>La Pampa</h2>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
